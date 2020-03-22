e76490a0314959e07d4403cc0f09368331126fec
huhn511 <sehe89@gmail.com>
2020-03-22 20:40:39 +0100

    fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'OMA - Online Marketplace Application',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
