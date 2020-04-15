b2e879b27128d2de369f0392d08e8f9b9afd2c1e
huhn511 <sehe89@gmail.com>
2020-04-16 01:20:52 +0200

    add translations

--- translations/de.js
+++ translations/de.js
@@ -1,18 +1,6 @@
 module.exports = {
-  home: {
-      title: 'The first einfachIOTA magazine.',
-  }, 
-  footer: {
-      title: 'Mehr',
-      blog: 'Blog',
-      magazine: 'Magazine',
-      community: 'Community',
-      partners: 'Partners',
-      iota_title: 'IOTA',
-      iota_web: 'Official Website',
-      iota_news: 'News',
-      iota_more: 'More',
-      imprint: 'Imprint',
-      privacy_policy: ' Privacy Policy'
-  },
+  greeting: 'Hallo Welt!',
+  imprint: require('./modules/imprint_de'),
+  privacy: require('./modules/privacy_de'),
+  donations: require('./modules/donations/de')
 }
