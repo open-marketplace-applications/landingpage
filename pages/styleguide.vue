b5d3eac23149f5248311af7eb7c508ca23c8f442
Sven Reiser <svreiser@gmail.com>
2020-04-25 21:51:42 +0200

    adds layout components to styleguide page

--- pages/styleguide.vue
+++ pages/styleguide.vue
@@ -1,37 +1,68 @@
 <template>
   <Page>
     <Section>
-      <Container>
-        <h1>Styleguide</h1>
-        <p>
-          Here you can find all components available in
-          open-marketplace/landing-page
-        </p>
-        <hr />
-        <h2>Button</h2>
-        <div>
-          <Button>Nothing</Button>
-          <Button v-bind:type="'default'">Default</Button>
-          <Button v-bind:type="'primary'">Primary</Button>
-          <Button v-bind:type="'darker'">Darker</Button>
-          <Button v-bind:type="'dark'">Dark</Button>
-          <Button v-bind:type="'light'">Light</Button>
-          <Button v-bind:type="'white'">White</Button>
-        </div>
+      <Container :size="'sm'">
+        <H1>Styleguide</H1>
+        <P
+          >Here you can find all components available in
+          open-marketplace/landing-page</P
+        >
+        <Hr />
+        <H2>Buttons</H2>
+        <P
+          >The button component allows you use it as a button with @clicked or
+          as a link with href.</P
+        >
+        <Row>
+          <Column :width="2">
+            <Button :type="'default'">Default</Button>
+          </Column>
+          <Column :width="2">
+            <Button :type="'primary'">Primary</Button>
+          </Column>
+          <Column :width="2">
+            <Button :type="'darker'">Darker</Button>
+          </Column>
+          <Column :width="2">
+            <Button :type="'dark'">Dark</Button>
+          </Column>
+          <Column :width="2">
+            <Button :type="'light'">Light</Button>
+          </Column>
+          <Column :width="2">
+            <Button :type="'white'">White</Button>
+          </Column>
+        </Row>
       </Container>
     </Section>
   </Page>
 </template>
 
 <script>
+// L I B R A R Y
 import Page from "../components/library/layout/Page";
 import Section from "../components/library/layout/Section";
 import Container from "../components/library/layout/Container";
+import Row from "../components/library/layout/Row";
+import Column from "../components/library/layout/Column";
+import H1 from "../components/library/typo/H1";
+import H2 from "../components/library/typo/H2";
+import P from "../components/library/typo/P";
+import Hr from "../components/library/typo/Hr";
 import Button from "../components/library/Button";
 
 export default {
-  components: { Page, Container, Section, Button }
+  components: {
+    Page,
+    Section,
+    Container,
+    Row,
+    Column,
+    H1,
+    H2,
+    P,
+    Hr,
+    Button
+  }
 };
 </script>
-
-<style></style>
