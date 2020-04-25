4d6296dcb3d2f401b8948fa30bdc9e6e4f8f50da
Sven Reiser <svreiser@gmail.com>
2020-04-25 19:45:07 +0200

    adds page component to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="page">
+  <Page>
     <Hero />
     <Map />
     <Apps />
     <JoinUs />
     <Socials />
     <Footer />
-  </div>
+  </Page>
 </template>
 
 <script>
+// L I B R A R Y
+import Page from "../components/library/layout/Page";
+
+// S E C T I O N S
 import Hero from "../components/Hero";
 import Map from "../components/Map.vue";
 import Apps from "../components/Apps.vue";
 import JoinUs from "../components/JoinUs.vue";
 import Socials from "../components/Socials";
 import Footer from "~/components/Footer.vue";
+
 export default {
-  components: { Hero, Map, Apps, JoinUs, Socials, Footer },
+  components: { Page, Hero, Map, Apps, JoinUs, Socials, Footer },
   data() {
     return {
       url: `${process.env.shopUrl}/en?refLink=${process.env.refAddress}`
