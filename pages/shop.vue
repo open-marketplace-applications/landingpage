88ce774880990c47e50eb5f0daeebd0a5632c856
huhn511 <sehe89@gmail.com>
2020-05-03 11:08:55 +0200

    add shop page

---
+++ pages/shop.vue
@@ -0,0 +1,31 @@
+<template>
+  <Page>
+    <Content :content="shop" />
+    <Footer />
+  </Page>
+</template>
+
+<script>
+// L I B R A R Y
+import Footer from "@/components/library/Footer.vue";
+import Content from "@/components/sections/Content";
+import Page from "@/components/library/layout/Page";
+
+
+export default {
+  components: {
+    Page,
+    Content,
+    Footer
+  },
+  data() {
+    return {
+      url: `${process.env.shopUrl}/en?refLink=${process.env.refAddress}`,
+      shop: {
+          title: 'Shop',
+          description: 'Open Marketplace Apps are decentralised open source apps for smart cities and local communities. With the shop application, users can sell products and goods without fees.'
+      }
+    };
+  }
+};
+</script>
