cce9b47d7186dc3d0f4283faebb5391ffc105df1
huhn511 <sehe89@gmail.com>
2020-10-09 21:28:13 +0200

    fix title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "Online Marketplace Applications",
+    title: "Open Marketplace Applications",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -15,7 +15,7 @@ module.exports = {
         hid: "description",
         name: "description",
         content:
-          "A free and feeless marketplace for a smart city - Connect sellers, delivery services & consumers"
+          "Open marketplace apps are decentralized open source apps for smart cities and local communities."
       }
     ],
     link: [
